a4ca3365255943d0543a918acdd26b23f5ee61ea
mikaeltv33n <mikaeltv33n>
2024-01-04 10:47:59 +0100

    apikey

--- src/components/weather-app/card.js
+++ src/components/weather-app/card.js
@@ -35,7 +35,7 @@ export default function Card({ lat, lon }) {
 
   useEffect(() => {
     async function getData() {
-      const apiKey = '371a743ceb6e25bf13659a362ab6c511'
+      const apiKey = process.env.NEXT_PUBLIC_API_KEY
       const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
       const data = await res.json()
 
